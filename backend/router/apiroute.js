6f06637ec19659fa8fb62b11f5f5a8c880ea338c
MHamzaAamir <121198646+MHamzaAamir@users.noreply.github.com>
2025-05-23 02:15:10 +0500

    minor updates

--- backend/router/apiroute.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router()
-const LLMController = require("../controller/llmController")
-
-router.post('/sendRequest',LLMController.sendRequest)
-
-
-module.exports = router;
